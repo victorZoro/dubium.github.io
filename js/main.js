a1281ff2abe7db3e19109c08c31ca8696ed2f234
Victor Gabriel <105755567+victorZoro@users.noreply.github.com>
2023-04-30 15:06:28 -0300

    Site quase completo
    Interface quase 100% completa. Apenas faltando a página de comentários.

---
+++ js/main.js
@@ -0,0 +1,41 @@
+const BODY = document.querySelector("body"),
+      SIDEBAR = document.querySelector(".sidebar"),
+      TOGGLE = document.querySelector(".toggle"),
+      USER_TEXT = document.querySelector(".user-text"),
+      STATUS = document.querySelector(".status"),
+      TOP_BAR = document.querySelector(".top-bar");
+
+let isClicked = false;
+
+TOGGLE.addEventListener("click", () => {
+    SIDEBAR.classList.toggle("close");
+})
+
+USER_TEXT.addEventListener("click", () => {
+    if(isClicked) {
+        STATUS.style.color = "var(--green-color)";
+        STATUS.innerText = "online";
+        isClicked = false;
+    } else {
+        STATUS.style.color = "var(--highlight-color)";
+        STATUS.innerText = "offline";
+        isClicked = true;    
+    }
+})
+
+window.addEventListener("resize", () => {
+    if(window.innerWidth <= 1024) {
+        SIDEBAR.classList.add("close");
+        TOGGLE.style.visibility = "hidden";
+    } else {
+        TOGGLE.style.visibility = "";
+    }
+})
+
+window.addEventListener("scroll", () => {
+    if(window.pageYOffset > 10) {
+        TOP_BAR.classList.add("scrolled");
+    } else {
+        TOP_BAR.classList.remove("scrolled");
+    }
+})
