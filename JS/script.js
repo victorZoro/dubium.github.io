466a75ef593513ae85b9155c716a79b83bc418a4
Victor Gabriel <105755567+victorZoro@users.noreply.github.com>
2023-05-28 21:39:18 -0300

    Mais atualizações na página inicial
    Parte onde o usuário faz posts foi adicionada. Infelizmente, a página inicial ainda não foi finalizada.

--- JS/script.js
+++ JS/script.js
@@ -4,3 +4,26 @@ const sidebar = document.querySelector('.sidebar');
 toggleBtn.addEventListener('click', () => {
     sidebar.classList.toggle('closed');
 });
+
+const toggleDarkModeBtn = document.querySelector('.light-dark-btn');
+
+toggleDarkModeBtn.addEventListener('click', () => {
+    if(toggleDarkModeBtn.classList.contains('light-mode')) {
+        toggleDarkModeBtn.innerHTML = 'clear_night';
+        document.body.classList.add('dark');
+        toggleDarkModeBtn.classList.remove('light-mode');
+        toggleDarkModeBtn.classList.add('dark-mode');
+    } else if(toggleDarkModeBtn.classList.contains('dark-mode')) {
+        toggleDarkModeBtn.innerHTML = 'brightness_7';
+        document.body.classList.remove('dark');
+        toggleDarkModeBtn.classList.remove('dark-mode');
+        toggleDarkModeBtn.classList.add('light-mode');
+    }
+});
+
+const sidebarSendBtn = document.querySelector('.focus-to-send');
+const sendPostBtn = document.querySelector('.send-input');
+
+sidebarSendBtn.addEventListener('click', () => {
+    sendPostBtn.focus();
+})
